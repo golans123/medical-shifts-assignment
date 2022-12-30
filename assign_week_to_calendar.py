59e57c0af698ab00776f1156e31173beae273042
golans123 <100952129+golans123@users.noreply.github.com>
2022-12-30 10:02:04 +0200

    first commit

---
+++ assign_week_to_calendar.py
@@ -0,0 +1,275 @@
+import numpy as np
+import pandas as pd
+import copy
+
+iteration = 0
+
+
+def create_empty_calendar(number_of_days_in_month, first_day_of_month):
+    """
+    a dataframe whose elements are lists cannot be deeply copied. hence the use
+     of tuples.
+     `None`` was used since pd.NA has an ambiguous boolean value.
+    :param number_of_days_in_month: int, 28 <= first_day_of_month <= 31
+    :param first_day_of_month: int, 0 <= first_day_of_month <= 6
+    :return:
+    """
+    days_of_the_week = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturaday"]
+    week_number = np.arange(6).astype(int)
+    current_day_of_month = 1
+    current_day_of_week = first_day_of_month
+    empty_calendar = pd.DataFrame(columns=days_of_the_week, index=week_number)
+    for i in week_number:
+        appended_days = [[j, None] for j in range(current_day_of_week, 7)]
+        empty_calendar.iloc[i, current_day_of_week:7] = appended_days
+        current_day_of_week = 0
+        current_day_of_month = current_day_of_month + len(appended_days)
+    # emptying days will always happen at the last week
+    if current_day_of_month > number_of_days_in_month:
+        number_of_days_to_delete = current_day_of_month - number_of_days_in_month - 1
+        if number_of_days_to_delete <= 7:
+            empty_calendar.iloc[-1, 7 - number_of_days_to_delete:7] = \
+                [None for j in range(number_of_days_to_delete)]
+        else:
+            empty_calendar.iloc[-1] = [None for j in range(7)]
+            empty_calendar.iloc[-2, 14 - number_of_days_to_delete:7] = \
+                [None for j in range(number_of_days_to_delete - 7)]
+    return empty_calendar
+
+
+def update_date(current_day_in_week, current_week_number, day_in_month, last_day_in_week, skip_tomorrow=False):
+    if not skip_tomorrow:
+        day_in_month += 1
+        if current_day_in_week == last_day_in_week:  # last_day_in_week = 6
+            current_day_in_week = 0
+            current_week_number += 1
+        else:
+            current_day_in_week += 1
+    else:
+        day_in_month += 2
+        if current_day_in_week == last_day_in_week:  # last_day_in_week = 6
+            current_day_in_week = 1
+            current_week_number += 1
+        elif current_day_in_week == (last_day_in_week - 1):
+            current_day_in_week = 0
+            current_week_number += 1
+        else:
+            current_day_in_week += 2
+    return current_day_in_week, current_week_number, day_in_month
+
+
+def create_calendar_with_shift_today(input_calendar, current_week_number,
+                                     current_day_in_week, intern_index, preferations_df, day_in_month):
+    # input_clanedar[1][0], input_clanedar[1][0], which are lists, should be deeply copied too
+    calendar_with_shift_today = [input_calendar[0].copy(deep=True),
+                                 [copy.deepcopy(input_calendar[1][0]), copy.deepcopy(input_calendar[1][1])],
+                                 input_calendar[2]]
+    calendar_with_shift_today[0].iloc[current_week_number].iloc[current_day_in_week]\
+        = [current_day_in_week, intern_index]
+    # update points
+    calendar_with_shift_today[1][0][intern_index] += preferations_df.iloc[intern_index][5 + day_in_month]
+    calendar_with_shift_today[1][1][intern_index] += 1
+    calendar_with_shift_today[2] += preferations_df.iloc[intern_index][5 + day_in_month]
+    return calendar_with_shift_today
+
+
+def update_calendars(input_calendar, intern_index, preferations_df, day_in_month,
+                     shift_number, shifts_to_do, current_week_number, current_day_in_week,
+                     last_day_in_week, first_day_of_month, number_of_days_in_month,
+                     calendars):
+    original_current_day_in_week, original_current_week_number, original_day_in_month = \
+        current_day_in_week, current_week_number, day_in_month
+    # todo: day +=2, can't do two shifts in a row
+    # with shift today, check if day is not already occupied
+    if input_calendar[0].iloc[current_week_number].iloc[current_day_in_week][1] is None:
+        calendar_with_shift_today = \
+            create_calendar_with_shift_today(input_calendar,
+                                             current_week_number,
+                                             current_day_in_week,
+                                             intern_index,
+                                             preferations_df, day_in_month)
+        current_day_in_week, current_week_number, day_in_month = \
+            update_date(current_day_in_week, current_week_number, day_in_month, last_day_in_week, skip_tomorrow=True)
+
+        calendars = assign_intern_recursively(shift_number + 1, shifts_to_do, current_week_number,
+                                              current_day_in_week,
+                                              last_day_in_week,
+                                              preferations_df, calendar_with_shift_today, intern_index,
+                                              first_day_of_month, day_in_month,
+                                              number_of_days_in_month, calendars)
+    # to avoid updating the date twice + different skip_tomorrow argument
+    current_day_in_week, current_week_number, day_in_month = \
+        update_date(original_current_day_in_week, original_current_week_number, original_day_in_month, last_day_in_week,
+                    skip_tomorrow=False)
+    # without shift today
+    calendar_without_shift_today = [input_calendar[0].copy(deep=True),
+                                    [input_calendar[1][0], input_calendar[1][1]], input_calendar[2]]
+    calendars = assign_intern_recursively(shift_number, shifts_to_do, current_week_number,
+                                          current_day_in_week,
+                                          last_day_in_week,
+                                          preferations_df, calendar_without_shift_today, intern_index,
+                                          first_day_of_month, day_in_month,
+                                          number_of_days_in_month, calendars)
+    return calendars
+
+
+def assign_intern_recursively(shift_number, shifts_to_do, current_week_number,
+                              current_day_in_week,
+                              last_day_in_week,
+                              preferations_df, input_calendar, intern_index,
+                              first_day_of_month, day_in_month,
+                              number_of_days_in_month, calendars):
+    """
+    there is no need to return calendars in the recursion since it's immutable
+    :param shift_number:
+    :param shifts_to_do:
+    :param current_week_number:
+    :param current_day_in_week:
+    :param last_day_in_week:
+    :param preferations_df:
+    :param input_calendar: (calendar, interns_points_list, total_points)
+    :param intern_index:
+    :param first_day_of_month:
+    :param day_in_month:
+    :param number_of_days_in_month:
+    :param calendars: (calendar, interns_points_list, total_points)
+    :return:
+    """
+    ###############
+    if globals()["iteration"] % 1000 == 0:
+        print(globals()["iteration"])
+    globals()["iteration"] += 1
+    ###############
+    if shift_number == shifts_to_do:
+        calendars.append(input_calendar)
+        return calendars
+    else:
+        # not in bans list
+        if day_in_month not in preferations_df.iloc[intern_index, 1:5].tolist():
+            # recursion stop condition - end of month (day_in_month >= 0)
+            # todo: == vs >=. day in month starts at zero, skip tomorrow.
+            if day_in_month >= number_of_days_in_month:
+                return calendars
+            else:  # continue recursion with and without a shift today
+                # update input calendar, create calendars to pass to recursion
+                calendars = \
+                    update_calendars(input_calendar, intern_index,
+                                     preferations_df, day_in_month, shift_number,
+                                     shifts_to_do, current_week_number, current_day_in_week,
+                                     last_day_in_week, first_day_of_month,
+                                     number_of_days_in_month, calendars)
+                return calendars
+        else:
+            # recursion stop condition - end of month (day_in_month >= 0)
+            # fixme: for the case where the last day is banned, checking arrival for the last day
+            #  would not suffice, so we need... complete. also, if input_calendar is already full
+            if ((day_in_month-1) == number_of_days_in_month) or (day_in_month == number_of_days_in_month):
+                return calendars
+            else:  # fixme
+                # without shift today
+                current_day_in_week, current_week_number, day_in_month = \
+                    update_date(current_day_in_week, current_week_number, day_in_month, last_day_in_week, skip_tomorrow=False)
+                calendar_without_shift_today = [input_calendar[0].copy(deep=True),
+                                                [input_calendar[1][0], input_calendar[1][1]], input_calendar[2]]
+                return assign_intern_recursively(shift_number, shifts_to_do, current_week_number,
+                                                 current_day_in_week, last_day_in_week,
+                                                 preferations_df, calendar_without_shift_today, intern_index,
+                                                 first_day_of_month, day_in_month,
+                                                 number_of_days_in_month, calendars)
+
+
+def assign_intern_to_all_possible_combinations(num_of_shifts,
+                                               calendars, last_day_in_week, preferations_df,
+                                               intern_index, first_day_of_month, number_of_days_in_month):
+    # reset calendars before each intern, since a calendar without all
+    # interns should not be saved to `calendars`
+    new_calendars = []
+    # min_points = np.inf
+    # try all possible amounts of shifts
+    for i in range(num_of_shifts):
+        for j in range(len(calendars)):
+            new_calendars = assign_intern_recursively(shift_number=0, shifts_to_do=i, current_week_number=0,
+                                                      current_day_in_week=0,
+                                                      last_day_in_week=last_day_in_week,
+                                                      preferations_df=preferations_df,
+                                                      input_calendar=calendars[j],
+                                                      intern_index=intern_index,
+                                                      first_day_of_month=first_day_of_month,
+                                                      day_in_month=0,
+                                                      number_of_days_in_month=number_of_days_in_month,
+                                                      calendars=new_calendars)
+            # fixme: this test line might need to catch more cases
+            # for the case where all calendars are full
+            if not new_calendars:
+                new_calendars = calendars
+    # assign to calendars the list of all most up-to-date calendars
+    return new_calendars
+
+
+def return_empty_weekly_calendar(first_day_of_week, last_day_of_week):
+
+    days_of_the_week = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturaday"]
+    days_of_the_week = days_of_the_week[first_day_of_week:last_day_of_week+1]
+    empty_calendar = pd.DataFrame(columns=days_of_the_week, index=[0])
+    appended_days = [[j, None] for j in range(first_day_of_week, last_day_of_week + 1)]
+    print(appended_days)
+    print(empty_calendar)
+    empty_calendar.iloc[0, first_day_of_week:last_day_of_week + 1] = appended_days
+    return empty_calendar
+
+
+def return_valid_calendars(preferations_df, max_num_of_shifts, min_num_of_shifts,
+                           number_of_days_in_month, first_day_of_month, last_day_in_week):
+    """
+    each cell of a calendar is a tuple (intern_name, day_num).
+
+    algorithm idea:
+    * using recursion according to the chosen number of shifts.
+    * no two
+    shifts in a row.
+
+    the theoretical amount of possible calendars in picking any combinations of
+    interns, with returns
+
+     the order of the recursion (which intern is first, second, etc.) is
+     important as interns might do a different amount of shifts (some 4,
+     some 5, some 6...). so instead of trying all orders, we try one order and
+     all possible amounts of shifts for each intern.
+
+     for later development:
+     * ER shifts are more expensive than department shifts.
+     * consider adding shift weight/points to the calendar-cell tuple in order
+     to choose best calendar.
+
+    :param last_day_in_week:
+    :param min_num_of_shifts:
+    :param preferations_df:
+    :param max_num_of_shifts:
+    :param number_of_days_in_month:
+    :param first_day_of_month:
+    :return:
+    """
+    # (calendar, (interns_points_list, interns_num_shifts_list), total_points)
+    calendars = [[return_empty_weekly_calendar(first_day_of_week, last_day_of_week),
+                  [np.zeros(preferations_df.shape[0]), np.zeros(preferations_df.shape[0])], 0]]
+    not_full_calendars = calendars
+    while not_full_calendars:
+        # for each calendar, while week is not full, for each intern, assign one shift per intern.
+        for i in range(0, preferations_df.shape[0]):
+            calendars = assign_intern_to_all_possible_combinations(num_of_shifts=1,
+                                                                   calendars=calendars,
+                                                                   last_day_in_week=last_day_in_week,
+                                                                   preferations_df=preferations_df, intern_index=i,
+                                                                   first_day_of_month=first_day_of_month,
+                                                                   number_of_days_in_month=number_of_days_in_month)
+    # assign each not-full calendar one shift of each intern at the time
+    # not_full_calendars = filter_not_full_calendars(calendars)
+    # use the not-full calendars
+    # full_calendars = [].append(filter_full_calendars(calendars, not_full_calendars))
+    # for i in range(max_num_of_shifts-min_num_of_shifts):
+    # assign each not-full calendar one shift of each intern at the time - re-check fullness.
+    return calendars
+
+
+if __name__ == '__main__':
+    return_empty_weekly_calendar(1, 3)
---
+++ main.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+
+
+import assign_week_to_calendar
+
+
+def normalize_points(preferations_df):
+    """
+    turn negative points to zeros.
+    :param preferations_df:
+    :return:
+    """
+    # rows 5 to 35 contain the points
+    row_sum = preferations_df.iloc[:, 5:36].sum(axis=1).to_numpy()[:, np.newaxis]
+    preferations_df.iloc[:, 5:36] = np.divide(preferations_df.iloc[:, 5:36], row_sum)
+
+
+def create_preferations_df(preferations_df_path):
+    preferations_df = pd.read_excel(preferations_df_path)
+    preferations_df["number_of_shifts"] = np.zeros(preferations_df.shape[0])  # todo
+    preferations_df["total_points"] = np.zeros(preferations_df.shape[0])
+    normalize_points(preferations_df)
+    return preferations_df
+
+
+def create_empty_calendar_test(number_of_days_in_month=5, first_day_of_month=0):
+
+    days_of_the_week = ["sunday", "monday", "tuesday", "wednesday", "thursday"]  # "friday", "saturaday"]
+    week_number = np.arange(1).astype(int)
+    current_day_of_month = 0
+    current_day_of_week = first_day_of_month
+    empty_calendar = pd.DataFrame(columns=days_of_the_week, index=week_number)
+    for i in week_number:
+        appended_days = [[j, None] for j in range(current_day_of_week, number_of_days_in_month)]
+        empty_calendar.iloc[i, current_day_of_week:number_of_days_in_month] = appended_days
+        current_day_of_week = 0
+        current_day_of_month = current_day_of_month + len(appended_days)
+    return empty_calendar
+
+
+# we use a while-loop instead of a for-loop because the number of
+#  iterations depends also on the bans lists, so it's hard to calculate
+def calendar_is_full(calendar):
+    for i in range(calendar.shape[0]):
+        for j in range(calendar.shape[1]):
+            if calendar.iloc[i, j][1] is None:
+                return False
+    return True
+
+
+if __name__ == '__main__':
+    main_preferations_df = create_preferations_df("preferations_df.xlsx")
+    # fixme: there is an error if not all can do minimal shifts
+    main_preferations_df = main_preferations_df.iloc[0:3]
+    main_calendars = assign_week_to_calendar.return_valid_calendars(
+        preferations_df=main_preferations_df,
+        max_num_of_shifts=2,
+        min_num_of_shifts=1,
+        number_of_days_in_month=5,
+        first_day_of_month=0,
+        last_day_in_week=4)
+
+    # print best 10 for each category:
+    # total points, uniform points dist., uniform num. shifts dist.
+    # lowest_points_calendars = np.array(main_calendars[2]).sort(axis=0)[:10]
+    # print(np.array(main_calendars)[:][2])
+    # lowest_points_calendars_indices = np.where(main_calendars == lowest_points_calendars)
+    # lowest_points_calendars = main_calendars[lowest_points_calendars_indices]
+    # for main_i in range(len(main_calendars)):
+    #     print(lowest_points_calendars[main_i][0])
+    #     print()
+    #     print(lowest_points_calendars[main_i][1][0])
+    #     print(lowest_points_calendars[main_i][1][1])
+    #     print(lowest_points_calendars[main_i][2])
+    #     print("\n")
+    for main_i in range(len(main_calendars)):
+        if calendar_is_full(main_calendars[main_i][0]):
+            print(main_calendars[main_i][0])
+            print()
+            print(main_calendars[main_i][1][0])
+            print(main_calendars[main_i][1][1])
+            print(main_calendars[main_i][2])
+            print("\n")
